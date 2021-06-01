90b7779dc4c2046ba2e023fea9e8fc8bf727642e
Jon Goldberg <jon@megaphonetech.com>
2021-06-01 15:22:14 -0400

    archive

--- CRM/Contribute/BAO/CheckNumberPaymentMethod.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class CRM_Contribute_BAO_CheckNumberPaymentMethod {
-
-  public static function getPaymentInstrument() {
-    return CRM_Contribute_BAO_Contribution::buildOptions('payment_instrument_id');
-  }
-
-}
